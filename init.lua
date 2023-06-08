6a503a3e48d107320a41fda206c22c3785255d5c
Grant Goodyear <grant.goodyear@bakerhughes.com>
2023-06-07 20:55:51 -0500

    adding null-ls, but not working yet

--- init.lua
+++ init.lua
@@ -429,7 +429,7 @@ end
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
 
---
+++ lua/custom/plugins/null-ls.lua
@@ -0,0 +1,14 @@
+-- null-ls
+
+return {
+  'jose-elias-alvarez/null-ls.nvim',
+  config = function()
+    local null_ls = require('null-ls')
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.ruff,
+      },
+    })
+  end,
+}
