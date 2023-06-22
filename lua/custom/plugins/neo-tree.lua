ce31c6fbcf6d6cd01dbc14ce5e9ebc82e773a00b
Grant Goodyear <grant.goodyear@bakerhughes.com>
2023-06-22 18:33:01 -0500

    add key binding for neo-tree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -5,8 +5,17 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim' },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
   config = function()
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
   end,
 }
-
